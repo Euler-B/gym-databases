eb7006615a78f30d1c902fef85a1c207637b72bd
Eduardo Bravo <eduardojosebb.matescience@gmail.com>
2024-02-03 22:45:16 -0400

    query for multiples rows

--- create-tables.sql
+++ create-tables.sql
@@ -11,11 +11,11 @@ CREATE TABLE album (
 INSERT INTO album
     (title, artist, price)
 VALUES
-    ('Coleccion Suprema', 'Los Prisioneros', 6.99)
-    ('Lo Mejor de Vilma Palma', 'Charly Garcia', 8.80)
-    ('La Ley MTV Unplugged', 'La Ley', 7.30)
-    ('Garcia 87/93', 'Charly Garcia', 8.50)
-    ('Cancion Animal', 'Soda Estereo', 8.00)
-    ('Signos', 'Soda Estereo', 9.00)
-    ('Sueños Liquidos', 'Soda Estereo', 9.00)
+    ('Coleccion Suprema', 'Los Prisioneros', 6.99),
+    ('Lo Mejor de Vilma Palma', 'Charly Garcia', 8.80),
+    ('La Ley MTV Unplugged', 'La Ley', 7.30),
+    ('Garcia 87/93', 'Charly Garcia', 8.50),
+    ('Cancion Animal', 'Soda Estereo', 8.00),
+    ('Signos', 'Soda Estereo', 9.00),
+    ('Sueños Liquidos', 'Soda Estereo', 9.00),
     ('Mil Siluetas', 'La Union', 7.00);
--- main.go
+++ main.go
@@ -33,6 +33,12 @@ func main() {
 		log.Fatal(pingErr)
 	}
 	fmt.Println("Connected!")
+
+	albums, err := albumsByArtist("Charly Garcia")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Albums found: %v\n", albums)
 }
 
 func init() {
@@ -40,4 +46,36 @@ func init() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+}
+
+type Album struct {
+	ID     int64
+	Title  string
+	Artist string
+	Price  float32
+}
+
+// albums By Artist queries for albums that have the spicified artist  name.
+
+func albumsByArtist(name string) ([]Album, error) {
+	// An albums slice to hold data from returned rows
+	var albums []Album
+	rows, err := db.Query("SELECT * FROM album WHERE artist = ?", name)
+	if err != nil {
+		return nil, fmt.Errorf("albumsByArtist %q:%v", name, err)
+	}
+	defer rows.Close()
+
+	// Loop through rows,using Scan to assign column data to struct fields
+	for rows.Next() {
+		var alb Album
+		if err := rows.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Title); err != nil {
+			return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+		}
+		albums = append(albums, alb)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+	}
+	return albums, nil
 }
